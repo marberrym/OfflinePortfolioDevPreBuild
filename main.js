c065865540c385f652efccab18919b28cba644fa
Matt <marberrym@gmail.com>
2018-08-20 17:03:55 -0400

    Added modal boxes.  Finished BG.  Need to work on modal population.

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
+let projects = document.querySelectorAll(".projectItem");
+let modal = document.querySelector(".modalWindow");
+let visibleModal = document.querySelector("#modalFlexOne");
+let modalDescription = document.querySelector("#modalFlexTwo");
 
-let colorSelectArray = ['rgb(127, 3, 3)', 
-                        'rgb(1, 14, 81)', 
-                        'rgb(1, 68, 81)',
-                        'rgb(107, 183, 7)',
-                        'rgb(214, 153, 0)',
-                        'rgb(0, 214, 157)',
-                        'rgb(214, 0, 92)',
-                        'rgb(188, 9, 12)',
-                        'rgb(90, 9, 188)',
-                        'rgb(182, 188, 9)']
+let hideModal = (event) => {
+    if (event.target === modal) {
+        modal.classList.remove("showModal");
+    }
+}
 
+projects.forEach(function(project) {
+    let modalPopup = function (event) {
+        modal.classList.add("showModal");
+        visibleModal.appendChild(modalIMG);
+        modalDescription.appendChild(title);
+    }
+    
+    let modalIMG = document.createElement('img');
+    let title = document.createElement('h1');
+    let imgSRC = project.getAttribute('src');
+    if (imgSRC.endsWith("HGASmall.png")) {
+        title.textContent = "Hidden Gem Atlanta";
+    }
+    modalIMG.setAttribute('src', imgSRC);
+    modalIMG.classList.add('modalIMG');
+    project.addEventListener('click', modalPopup);
+})
 
-var words = document.querySelectorAll(".wordCloud")
-console.log(words);
+window.addEventListener('click', hideModal);
 
-words.forEach(function(word) {
-    let colorSelector = Math.floor(Math.random() * 10);
-    word.setAttribute("color", colorSelectArray[colorSelector - 1]);
-    console.log(colorSelector);
-});
