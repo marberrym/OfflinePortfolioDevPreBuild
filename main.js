47caf9bbc3abadb9c689730c2c1d2d90bb2a5ea9
Matt <marberrym@gmail.com>
2018-08-20 23:23:59 -0400

    Added modal functionality, need to add transitional effects and some other stuff.  Getting much farther along though.  Need to be able to remove DOM elements from my modal as well.

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
+let projectObject = {
+    1: {
+        title: "Hidden Gem Atlanta",
+        description: "Hidden Gem Atlanta is a fully functional app which thrives off of user generated content. \
+        HGA aims to aid in the growth and sustainability of local 'gems' in a day where large organizations dominate the marketplace."
+    }
+}
+
 let projects = document.querySelectorAll(".projectItem");
 let modal = document.querySelector(".modalWindow");
 let visibleModal = document.querySelector("#modalFlexOne");
-let modalDescription = document.querySelector("#modalFlexTwo");
+let modalTitle = document.querySelector("#modalFlexTwo");
+
 
 let hideModal = (event) => {
     if (event.target === modal) {
@@ -13,14 +22,17 @@ projects.forEach(function(project) {
     let modalPopup = function (event) {
         modal.classList.add("showModal");
         visibleModal.appendChild(modalIMG);
-        modalDescription.appendChild(title);
+        modalTitle.appendChild(title);
+        modalTitle.appendChild(description);
     }
     
     let modalIMG = document.createElement('img');
     let title = document.createElement('h1');
+    let description = document.createElement('p')
     let imgSRC = project.getAttribute('src');
     if (imgSRC.endsWith("HGASmall.png")) {
-        title.textContent = "Hidden Gem Atlanta";
+        title.textContent = projectObject[1]["title"]
+        description.textContent = projectObject[1]["description"]
     }
     modalIMG.setAttribute('src', imgSRC);
     modalIMG.classList.add('modalIMG');
