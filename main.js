7c0221e17ec6e63bce09842e260e4e2fd7485a96
Matt <marberrym@gmail.com>
2018-08-23 13:16:05 -0400

    Working on getting modal fully functional.

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 let projectObject = {
     1: {
+        src: '../PortfolioFD/images/HGASmall.png',
         title: "Hidden Gem Atlanta",
         description: "Hidden Gem Atlanta is a fully functional app which thrives off of user generated content. \
         HGA aims to aid in the growth and sustainability of local 'gems' in a day where large organizations dominate the marketplace."
+    },
+    2: {
+        src: '../PortfolioFD/images/MMLogo.png',
+        title: "Hidden Gem Atlanta",
+        description: "Hidden Gem Atlanta is a fully functional app which thrives off of user generated content. \
+        HGA aims to aid in the growth and sustainability of local 'gems' in a day where large organizations dominate the marketplace."
+    },
+    3: {
+        src: '../PortfolioFD/images/HGA.png',
+        title: "Hidden Gem Atlanta",
+        description: "BLABLAHBLAHBLAHBLAHBKLAH"
     }
 }
 
@@ -33,9 +45,18 @@ projects.forEach(function(project) {
     let title = document.createElement('h1');
     let description = document.createElement('p')
     let imgSRC = project.getAttribute('src');
-    if (imgSRC.endsWith("HGASmall.png")) {
+
+    console.log(imgSRC);
+
+    if (imgSRC === projectObject[1]['src']) {
         title.textContent = projectObject[1]["title"]
         description.textContent = projectObject[1]["description"]
+    } else if (imgSRC === projectObject[2]['src']) {
+        title.textContent = projectObject[2]["title"]
+        description.textContent = projectObject[2]["description"]
+    } else if (imgSRC === projectObject[3]['src']) {
+        title.textContent = projectObject[3]["title"]
+        description.textContent = projectObject[3]["description"]
     }
     modalIMG.setAttribute('src', imgSRC);
     modalIMG.classList.add('modalIMG');
